374325ac47d349413dadce5af5a2136d29b21133
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2024-07-22 22:14:26 -0300

    feat: image rota

--- start/routes.ts
+++ start/routes.ts
@@ -8,6 +8,8 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { sep, normalize } from 'node:path'
+import app from '@adonisjs/core/services/app'
 
 import '../routes/user.routes.ts'
 import '../routes/user_type.routes.ts'
@@ -25,4 +27,18 @@ import ws from '#services/ws'
 router.get('/:id', async ({ response, params }) => {
   ws.io?.emit(`notify.${params.id}`, { message: 'Hello, World' })
   return response.send({ message: `send message to ${params.id}` })
+})
+
+const PATH_TRAVERSAL_REGEX = /(?:^|[\\/])\.\.(?:[\\/]|$)/
+
+router.get('/uploads/*', ({ request, response }) => {
+  const filePath = request.param('*').join(sep)
+  const normalizedPath = normalize(filePath)
+
+  if (PATH_TRAVERSAL_REGEX.test(normalizedPath)) {
+    return response.badRequest('Malformed path')
+  }
+
+  const absolutePath = app.makePath('uploads', normalizedPath)
+  return response.download(absolutePath)
 })
